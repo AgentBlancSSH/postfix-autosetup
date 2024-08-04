13cd940ed532d4b2bca7575569efd0c90f2916ca
AgentBlancSSH <177425734+AgentBlancSSH@users.noreply.github.com>
2024-08-04 03:15:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Update the package list
-sudo apt-get update
-
-# Upgrade the installed packages
+# Mise à jour du système
+sudo apt-get update -y
 sudo apt-get upgrade -y
-
-# Perform a full upgrade
 sudo apt-get full-upgrade -y
-
-# Install packages
 sudo apt-get install -y python3-pip curl nano python3
 
 echo "System update, upgrade, and installations completed!"
 
-# Ask the user if they want to run postfix.py
+# Demander à l'utilisateur s'il veut exécuter postfix.py
 read -p "Lancer l'installation de postfix? (yes/no): " choice
 
 if [ "$choice" == "yes" ] || [ "$choice" == "y" ]; then
