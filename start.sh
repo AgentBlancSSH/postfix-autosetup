f46495dae11de09bcca5937e81fdfb15fbfb96e2
Casual_Kitt3n / SereniTeaWithKetamine <49318780+casualkitten1@users.noreply.github.com>
2024-08-04 02:32:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,3 +13,16 @@ sudo apt-get full-upgrade -y
 sudo apt-get install -y python3-pip curl nano python3
 
 echo "System update, upgrade, and installations completed!"
+
+# Ask the user if they want to run postfix.py
+read -p "Lancer l'installation de postfix? (yes/no): " choice
+
+if [ "$choice" == "yes" ] || [ "$choice" == "y" ]; then
+    if [ -f "postfix.py" ]; then
+        python3 postfix.py
+    else
+        echo "postfix.py not found!"
+    fi
+else
+    echo "Skipping the execution of postfix.py."
+fi
